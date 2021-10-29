fc8311400b96b6184dc87fd8483729cf13720ea3
Silvi Gabot <king@foo.com>
2021-10-29 09:12:29 -0400

    Working version :) Custom Controller printing number of pods in the cluster

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM golang:1.13-alpine
-RUN mkdir /app
-ADD . /app
-WORKDIR /app
+ADD . /
+WORKDIR /
 RUN GOOS=linux GOFLAGS=-mod=vendor go build -o ./app .
 
 FROM alpine
 COPY --from=0 ./app /app
+RUN chmod a+x /app
 ENTRYPOINT /app
--- main.go
+++ main.go
@@ -1,16 +1,3 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 // Note: the example only works with the code within the same release/branch.
 package main
 
@@ -19,7 +6,6 @@ import (
 	"fmt"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -57,16 +43,16 @@ func main() {
 		// Examples for error handling:
 		// - Use helper functions e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found example-xxxxx pod in default namespace\n")
-		}
+		// _, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		// if errors.IsNotFound(err) {
+		// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		// } else if err != nil {
+		// 	panic(err.Error())
+		// } else {
+		// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		// }
 
 		time.Sleep(10 * time.Second)
 	}
