ae6c57baed059b026586cace5f0fe8e94eac0b5f
Amirsil <57900754+Amirsil@users.noreply.github.com>
2021-11-04 12:30:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -39,21 +39,6 @@ func main() {
 			panic(err.Error())
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		// _, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-		// if errors.IsNotFound(err) {
-		// 	fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		// } else if err != nil {
-		// 	panic(err.Error())
-		// } else {
-		// 	fmt.Printf("Found example-xxxxx pod in default namespace\n")
-		// }
-
 		time.Sleep(1 * time.Second)
 	}
 }
