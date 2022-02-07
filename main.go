83540fa4864074b984d8b3f178646895ec5e935e
Amirsil <57900754+Amirsil@users.noreply.github.com>
2022-02-07 09:20:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/pkg/api/v1"
+        "k8s.io/client-go/tools/clientcmd"
+    	"k8s.io/client-go/tools/cache"
+     	"k8s.io/client-go/pkg/fields"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -31,14 +35,30 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-		time.Sleep(1 * time.Second)
-	}
+	watchlist := cache.NewListWatchFromClient(
+		clientset.Core().RESTClient(), 
+		"services", v1.NamespaceDefault,
+        	fields.Everything())
+	
+        _, controller := cache.NewInformer(
+		watchlist,
+		&v1.Service{},
+		time.Second * 0,
+		cache.ResourceEventHandlerFuncs{
+		    AddFunc: func(obj interface{}) {
+			fmt.Printf("service added: %s \n", obj)
+		    },
+		    DeleteFunc: func(obj interface{}) {
+			fmt.Printf("service deleted: %s \n", obj)
+		    },
+		    UpdateFunc:func(oldObj, newObj interface{}) {
+			fmt.Printf("service changed \n")
+		    },
+	        }
+	)
+        stop := make(chan struct{})
+        go controller.Run(stop)
+        for {
+	    time.Sleep(time.Second)
+        }
 }
